4fb3ebbe46211f5722064a3c66fa4dd19fb1b3b7
Mikhail Tsyvis <mikhail_tsyvis@epam.com>
2019-09-30 17:50:49 +0300

    Fixed envitonment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import 'zone.js/dist/zone-error';
 
 export const environment = {
   production: false,
-  apiUrl: 'https://katla-sport-back-end.azurewebsites.net',
+  apiUrl: 'https://katla-sport-back-end.azurewebsites.net/',
 };
