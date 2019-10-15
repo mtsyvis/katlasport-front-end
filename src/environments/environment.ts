7a0fd5c802127977b3fd8eedbef9b4d3b4c37f0a
Mikhail Tsyvis <mikhail_tsyvis@epam.com>
2019-10-15 12:16:03 +0300

    changed enviroment to realize

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import 'zone.js/dist/zone-error';
 
 export const environment = {
   production: false,
-  //apiUrl: 'https://katla-sport-back-end.azurewebsites.net/',
-  apiUrl: 'http://localhost:56952/'
+  apiUrl: 'https://katla-sport-back-end.azurewebsites.net/',
+  // apiUrl: 'http://localhost:56952/'
 };
